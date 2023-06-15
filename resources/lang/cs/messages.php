08ac89809ad0eaa5d1dff9bf1ec67ecb45b58444
Stjepan Čagalj <32268075+scagalj@users.noreply.github.com>
2023-06-15 16:04:13 +0200

    Update messages.php

--- resources/lang/cs/messages.php
+++ resources/lang/cs/messages.php
@@ -8,7 +8,7 @@ return [
     'Contact' => 'Kontakt',
     'ApartmentsNature' => 'Apartments Nature',
     'ApartmentsNatureTitle' => 'Apartments Nature | Omiš',
-    'ApartmentsNatureDescription' => 'Najděte si svůj pronájem bytu v Omisu poblíž centra města, moře a krásné přírody. Ideální pro přátele, páry a rodiny na krátkodobé i dlouhodobé pobyty.',
+    'ApartmentsNatureDescription' => 'Najděte si svůj pronájem bytu v Omiš poblíž centra města, moře a krásné přírody. Ideální pro přátele, páry a rodiny na krátkodobé i dlouhodobé pobyty.',
     'Guests' => 'Hosté',
     'Bedrooms' => 'Ložnice',
     'Beds' => 'Postele',
